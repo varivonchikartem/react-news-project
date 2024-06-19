c0c279b34096b30601be9a684ce9214d081362f2
Varivonchik Artem <varivonchikartem.business@gmail.com>
2024-06-19 14:12:04 +0300

    Create Button shared basic component

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import clx from "classnames";
+import styles from "./Button.module.scss";
+
+export enum ButtonTheme {
+  DEFAULT = "default_theme",
+}
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+
+  children: React.ReactNode;
+  theme: ButtonTheme;
+}
+
+function Button(props: ButtonProps) {
+  const { className, children, theme, ...otherProps } = props;
+
+  const buttonClasses = clx(styles.Button, {
+    [className!]: className,
+    [styles[theme]]: theme,
+  });
+
+  return (
+    <button className={buttonClasses} {...otherProps}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
