1fecdd2003c65c129a4361e5b761beca439149a3
Varivonchik Artem <varivonchikartem.business@gmail.com>
2024-06-19 04:05:45 +0300

    Delete Footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import clx from "classnames";
 import styles from "./App.module.scss";
+
 import { Header } from "../widgets/Header";
 import { Sidebar } from "../widgets/Sidebar";
-import { PageWrapper } from "../widgets/PageWrapper/index.";
-import { AppRoutes } from "../shared/RouterConfiguration/RouterConfiguration";
 import { AppRouter } from "./providers/AppRouter";
-import { Footer } from "../widgets/Footer";
 
 interface AppProps {
   className?: string;
@@ -24,7 +22,6 @@ export const App: React.FC<AppProps> = (props) => {
       <Header />
       <Sidebar />
       <AppRouter />
-      <Footer />
     </div>
   );
 };
