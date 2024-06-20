be7537d74e805f20fcd33463aae8a99a5b9e3f41
Varivonchik Artem <varivonchikartem.business@gmail.com>
2024-06-21 02:09:18 +0300

    Create Avatar shared component

---
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import clx from "classnames";
+import styles from "./Avatar.module.scss";
+
+interface AvatarProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  className?: string;
+
+  height?: string;
+  width?: string;
+  borderRadius: number;
+
+  fallback?: React.ReactElement;
+  errorFallback?: React.ReactElement;
+}
+
+export function Avatar(props: AvatarProps) {
+  const { className, src, alt, height, width, borderRadius, fallback, errorFallback, ...otherProps } = props;
+
+  const avatarClasses = clx(styles.Avatar, {
+    [className!]: className,
+  });
+
+  const avatarSizesStyles = React.useMemo<React.CSSProperties>(() => {
+    return {
+      height: height || 20,
+      width: width || 20,
+      borderRadius: borderRadius,
+    };
+  }, [height, width]);
+
+  return <img className={avatarClasses} style={avatarSizesStyles} src={src} alt={alt} {...otherProps} />;
+}
