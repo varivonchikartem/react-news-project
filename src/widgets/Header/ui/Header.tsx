b4e7d2a5ff0cba3177e09434b8d9373d4efbab07
Varivonchik Artem <varivonchikartem.business@gmail.com>
2024-06-21 03:18:16 +0300

    Fix DropDown styles

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -4,6 +4,12 @@ import styles from "./Header.module.scss";
 import { ThemeSwitcher } from "../../../features/ThemeSwitcher/ui/ThemeSwitcher";
 import { AuthorizationModal } from "../../../features/Authorization";
 import Button, { ButtonTheme } from "../../../shared/ui/Button/Button";
+import { useSelector } from "react-redux";
+import { getUserAuthenticationData } from "../../../entities/User/model/selectors/getUserAuthenticationData/getUserAuthenticationData";
+import DropDown from "../../../shared/ui/DropDown/DropDown";
+import { Avatar } from "../../../shared/ui/Avatar/Avatar";
+import { useAppDispatch } from "../../../shared/lib/hooks/useAppDispatch/useAppDispatch";
+import { UserActions } from "../../../entities/User";
 
 interface HeaderProps {
   className?: string;
@@ -16,6 +22,9 @@ export const Header: React.FC<HeaderProps> = (props) => {
     [className!]: className,
   });
 
+  const dispatch = useAppDispatch();
+  const userAuthenticationData = useSelector(getUserAuthenticationData);
+
   const [isAuthModal, setIsAuthModal] = React.useState(false);
 
   const onOpenModal = React.useCallback(() => {
@@ -24,6 +33,10 @@ export const Header: React.FC<HeaderProps> = (props) => {
 
   const onCloseModal = React.useCallback(() => {
     setIsAuthModal(false);
+  }, []);
+
+  const onLogout = React.useCallback(() => {
+    dispatch(UserActions.logout());
   }, []);
 
   return (
@@ -39,10 +52,37 @@ export const Header: React.FC<HeaderProps> = (props) => {
           </form>
           <div className={styles.header_panel}>
             <ThemeSwitcher />
-            {isAuthModal && <AuthorizationModal onOpenModal={isAuthModal} onCloseModal={onCloseModal} />}
-            <Button theme={ButtonTheme.DEFAULT} onClick={onOpenModal}>
-              Log in
-            </Button>
+            {userAuthenticationData && (
+              <>
+                <DropDown
+                  trigger={
+                    <Avatar
+                      src="https://images.pexels.com/photos/19101653/pexels-photo-19101653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                      alt="image"
+                      width="31px"
+                      height="31px"
+                      borderRadius={30}
+                    />
+                  }
+                  items={[
+                    {
+                      content: "Выйти",
+                      onClick: onLogout,
+                    },
+                  ]}
+                  menuPosition="33px 0 auto auto"
+                  minWidth="300px"
+                />
+              </>
+            )}
+            {!userAuthenticationData && (
+              <>
+                <Button theme={ButtonTheme.DEFAULT} onClick={onOpenModal}>
+                  Log in
+                </Button>
+                {isAuthModal && <AuthorizationModal onOpenModal={isAuthModal} onCloseModal={onCloseModal} />}
+              </>
+            )}
           </div>
         </div>
       </div>
