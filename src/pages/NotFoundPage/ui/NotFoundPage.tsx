2337d1567c4d7f82423b8675f46eb7d8b54bea0d
Varivonchik Artem <varivonchikartem.business@gmail.com>
2024-07-03 13:54:12 +0300

    Prepare the structure and hierarchy of styles for responsive design in the pages - NotFoundPage

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -16,20 +16,15 @@ const NotFoundPage: React.FC<NotFoundPageProps> = (props) => {
 
   return (
     <div className={notfoundpageClasses}>
-      <div className={styles.main}>
-        <div className={styles.main1}>
+      <div className={styles.NotFoundPageInner}>
+        <div className={styles.Description}>
           <h1>This page doesn’t exist. But you exist.</h1>
           <p className={styles.subtitle}>
             This is your sign to take a break. And take a breath. In and out. When you’re ready, ease back to
             work below.
           </p>
         </div>
-        <div className={styles.main2}>
-          <AppImage
-            minHeight="700px"
-            src="https://media.sproutsocial.com/uploads/404-Space-Illustration.svg"
-          />
-        </div>
+        <AppImage minHeight="700px" src="https://media.sproutsocial.com/uploads/404-Space-Illustration.svg" />
       </div>
     </div>
   );
